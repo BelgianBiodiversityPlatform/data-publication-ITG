3a5913e0169b7286209ec11250556cbe25e192f8
DimEvil <dimibrosens@gmail.com>
2018-07-27 16:27:39 +0200

    update sql sampling

--- ITG ExoSurv Mosquito 2012/SCR/MMLPSampling.sql
+++ ITG ExoSurv Mosquito 2012/SCR/MMLPSampling.sql
@@ -1,8 +1,8 @@
-Drop view exosurvMMLPSampling;
-create view exosurvMMLPSampling as
+Drop view exosurvMMLPLatinSampling;
+create view exosurvMMLPLatinSampling as
 
 SELECT *		
 		
 FROM IdentificationMMLP
-	 LEFT join SamplingMMLP  ON  SamplingMMLP.SamplingCode = IdentificationMMLP.SamplingCode
-	 LEFT join SiteForm ON SiteForm.SiteCode = SamplingMMLP.SiteCode
+	 LEFT join MMLP_LatinSamplingJoined  ON  MMLP_LatinSamplingJoined.SamplingCode = IdentificationMMLP.SamplingCode
+	 LEFT join SiteForm ON SiteForm.SiteCode = MMLP_LatinSamplingJoined.SiteCode
